a40c01c47dc0916d18e9a306187ed857181fc567
tmslvk <timofeyselyavkomain@gmail.com>
2024-08-26 19:48:19 +0300

    Fix

--- OCB-Program/webapi/Model/Category.cs
+++ OCB-Program/webapi/Model/Category.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public List<Results> Results { get; set; }
+        public List<Outlay> Outlays { get; set; }
     }
 }
